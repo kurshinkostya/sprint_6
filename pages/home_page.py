4d29a744c20b83dac389e6e63a0d9467d3a3eb53
Konstantin Kurshin <ekurshin@gmail.com>
2025-10-07 23:32:56 +0300

    fix pages

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,36 +1,47 @@
 import allure
 from pages.base_page import BasePage
 from utils.locators import BasePageLocator
-from selenium.webdriver.support.wait import WebDriverWait
 from utils.locators import YaScooterHomePageLocator as Locators
-from selenium.webdriver.support import expected_conditions as EC
+from utils.urls import Urls
 
 
 class YaScooterHomePage(BasePage):
-    @allure.step('Нажать на кнопку заказа вверху страницы')
+
+    @allure.step("Нажать на кнопку заказа вверху страницы")
     def click_top_order_button(self):
-        return self.find_element(Locators.TOP_ORDER_BUTTON).click()
+        self.find_element(Locators.TOP_ORDER_BUTTON).click()
 
-    @allure.step('Нажать на кнопку заказа внизу страницы')
+    @allure.step("Нажать на кнопку заказа внизу страницы")
     def click_bottom_order_button(self):
-        return self.find_element(Locators.BOTTOM_ORDER_BUTTON).click()
+        self.find_element(Locators.BOTTOM_ORDER_BUTTON).click()
 
-    @allure.step('Нажать на вопрос в FAQ')
+    @allure.step("Нажать на вопрос {question_number} в разделе FAQ")
     def click_faq_question(self, question_number: int):
-        elems = self.find_elements(Locators.FAQ_BUTTONS, 10)
-        return elems[question_number].click()
+        elems = self.find_elements(Locators.FAQ_BUTTONS)
+        elems[question_number].click()
 
-    @allure.step('Переключиться на вкладку браузера')
+    @allure.step("Переключиться на вкладку {window_number}")
     def switch_window(self, window_number: int = 1):
-        return self.driver.switch_to.window(self.driver.window_handles[window_number])
+        self.switch_to_window(window_number)
 
+    @allure.step("Ожидать, пока URL перестанет быть 'about:blank'")
     def wait_url_until_not_about_blank(self, time=10):
-        return WebDriverWait(self.driver, time).until_not(EC.url_to_be('about:blank'))
+        self.wait_until_url_not_blank(time)
 
-    @allure.step('Перейти на страницу яндекса')
+    @allure.step("Перейти на страницу Яндекса")
     def click_yandex_button(self):
-        return self.find_element(BasePageLocator.YANDEX_SITE_BUTTON).click()
+        self.find_element(BasePageLocator.YANDEX_SITE_BUTTON).click()
 
-    @allure.step('Принять куки')
+    @allure.step("Принять куки")
     def click_cookie_accept(self):
-        return self.find_element(BasePageLocator.COOKIE_ACCEPT_BUTTON).click()
+        self.find_element(BasePageLocator.COOKIE_ACCEPT_BUTTON).click()
+
+    @allure.step("Проверить, что открыт допустимый URL")
+    def is_valid_redirect(self):
+        current_url = self.current_url()
+        valid_urls = [
+            Urls.YANDEX_HOME_PAGE,
+            Urls.DZEN_HOME_PAGE,
+            Urls.YANDEX_CAPTCHA_PAGE
+        ]
+        return any(url in current_url for url in valid_urls)
--- pages/order_page.py
+++ pages/order_page.py
@@ -1,82 +1,86 @@
-import re
-
+import allure
 from pages.base_page import BasePage
 from utils.locators import YaScooterOrderPageLocator as Locators
-import allure
-
 
 class YaScooterOrderPage(BasePage):
-    @allure.step('Ввод фамилии')
-    def input_last_name(self, last_name: str):
-        return self.find_element(Locators.LAST_NAME_INPUT).send_keys(last_name)
 
-    @allure.step('Ввод имени')
-    def input_first_name(self, first_name: str):
-        return self.find_element(Locators.FIRST_NAME_INPUT).send_keys(first_name)
+    # ввод пользовательских данных 
+    @allure.step("Ввести имя: {first_name}")
+    def input_first_name(self, first_name):
+        self.find_element(Locators.FIRST_NAME_INPUT).send_keys(first_name)
 
-    @allure.step('Ввод адреса')
-    def input_address(self, address: str):
-        return self.find_element(Locators.ADDRESS_INPUT).send_keys(address)
+    @allure.step("Ввести фамилию: {last_name}")
+    def input_last_name(self, last_name):
+        self.find_element(Locators.LAST_NAME_INPUT).send_keys(last_name)
 
-    @allure.step('Выбор метро')
-    def choose_subway(self, subway_name: str):
-        self.find_element(Locators.SUBWAY_FIELD).click()
-        return self.find_element(Locators.SUBWAY_HINT_BUTTON(subway_name)).click()
+    @allure.step("Ввести адрес: {address}")
+    def input_address(self, address):
+        self.find_element(Locators.ADDRESS_INPUT).send_keys(address)
 
-    @allure.step('Ввод номера телефона')
-    def input_telephone_number(self, telephone_number: str):
-        return self.find_element(Locators.TELEPHONE_NUMBER_FIELD).send_keys(telephone_number)
+    @allure.step("Ввести метро: {subway}")
+    def input_subway(self, subway):
+        self.find_element(Locators.SUBWAY_FIELD).send_keys(subway)
+        self.find_element(Locators.SUBWAY_HINT_BUTTON(subway)).click()
 
-    @allure.step('Перейти на следующий этап заказа')
+    @allure.step("Ввести телефон: {telephone}")
+    def input_telephone_number(self, telephone):
+        self.find_element(Locators.TELEPHONE_NUMBER_FIELD).send_keys(telephone)
+
+    @allure.step("Нажать 'Далее'")
     def go_next(self):
-        return self.find_element(Locators.NEXT_BUTTON).click()
+        self.find_element(Locators.NEXT_BUTTON).click()
 
-    @allure.step('Ввод даты')
-    def input_date(self, date: str):
-        return self.find_element(Locators.DATE_FIELD).send_keys(date)
+    # ввод аренды 
+    @allure.step("Заполнить данные аренды")
+    def fill_rent_data(self, data):
+        # выбор даты
+        self.find_element(Locators.DATE_FIELD).send_keys(data['date'])
 
-    @allure.step('Выбор периода аренды')
-    def choose_rental_period(self, option: int):
+        # выбор периода аренды
         self.find_element(Locators.RENTAL_PERIOD_FIELD).click()
-        return self.find_elements(Locators.RENTAL_PERIOD_LIST)[option].click()
+        options = self.find_elements(Locators.RENTAL_PERIOD_LIST)
+        options[data['rental_period']].click()
 
-    @allure.step('Выбор цвета')
-    def choose_color(self, option: int):
-        return self.find_elements(Locators.COLOR_CHECKBOXES)[option].click()
+        # выбор цвета
+        checkboxes = self.find_elements(Locators.COLOR_CHECKBOXES)
+        for index in data['color']:
+            checkboxes[index].click()
 
-    @allure.step('Комментарий для курьера')
-    def input_comment(self, comment_text):
-        return self.find_element(Locators.COMMENT_FOR_COURIER_FIELD).send_keys(comment_text)
+        # комментарий для курьера
+        self.find_element(Locators.COMMENT_FOR_COURIER_FIELD).send_keys(data['comment_for_courier'])
 
-    @allure.step('Нажать "Заказать"')
+    @allure.step("Нажать 'Заказать'")
     def click_order(self):
-        return self.find_element(Locators.ORDER_BUTTON).click()
+        self.find_element(Locators.ORDER_BUTTON).click()
 
-    @allure.step('Подтвердить заказ')
+    @allure.step("Принять заказ")
     def click_accept_order(self):
-        return self.find_element(Locators.ACCEPT_ORDER_BUTTON).click()
+        self.find_element(Locators.ACCEPT_ORDER_BUTTON).click()
 
-    @allure.step('Вычитать номер заказа')
+    @allure.step("Получить номер заказа")
     def get_order_number(self):
-        about_order_text = self.find_element(Locators.ORDER_COMPLETED_INFO).text
-        return ''.join(re.findall('[0-9]', about_order_text))
+        return self.find_element(Locators.ORDER_COMPLETED_INFO).text
 
-    @allure.step('Перейти к статусу заказа')
-    def click_go_to_status(self):
-        return self.find_element(Locators.SHOW_STATUS_BUTTON).click()
+    @allure.step("Заполнить данные пользователя")
+    def fill_user_data(self, data):
+        self.input_first_name(data['first_name'])
+        self.input_last_name(data['last_name'])
+        self.input_address(data['address'])
+        self.input_subway(data['subway_name'])
+        self.input_telephone_number(data['telephone'])
 
-    @allure.step('Заполнить данные на этапе "Для кого самокат"')
-    def fill_user_data(self, data_set: dict):
-        self.input_first_name(data_set['first_name'])
-        self.input_last_name(data_set['last_name'])
-        self.input_address(data_set['address'])
-        self.choose_subway(data_set['subway_name'])
-        self.input_telephone_number(data_set['telepthone_number'])
+    # проверка ошибок
+    def is_first_name_error_displayed(self):
+        return self.is_element_visible(Locators.INCORRECT_FIRST_NAME_MESSAGE)
 
-    @allure.step('Заполнить данные на этапе "Про аренду"')
-    def fill_rent_data(self, data_set: dict):
-        self.input_date(data_set['date'])
-        self.choose_rental_period(data_set['rental_period'])
-        for option in data_set['color']:
-            self.choose_color(option)
-        self.input_comment(data_set['comment_for_courier'])
+    def is_last_name_error_displayed(self):
+        return self.is_element_visible(Locators.INCORRECT_LAST_NAME_MESSAGE)
+
+    def is_address_error_displayed(self):
+        return self.is_element_visible(Locators.INCORRECT_ADDRESS_MESSAGE)
+
+    def is_subway_error_displayed(self):
+        return self.is_element_visible(Locators.INCORRECT_SUBWAY_MESSAGE)
+
+    def is_telephone_error_displayed(self):
+        return self.is_element_visible(Locators.INCORRECT_TELEPHONE_NUMBER_MESSAGE)
